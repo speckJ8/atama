32203277e241eefa8ac209f9455da7c3650eae89
Alcides Andrade <andrade.alcides.junior@gmail.com>
2022-03-02 19:24:16 +0900

    add name to processor core

--- device/core.go
+++ device/core.go
@@ -23,15 +23,17 @@ type Core struct {
 	// adder    Adder
 	// multiplier Multiplier
 	// divider Divider
+	Name      string
 	Mode      CoreMode
 	Status    CoreStatus
 	Registers RegisterSet
 }
 
-func NewCore(proc *Processor, mem *Memory) Core {
+func NewCore(proc *Processor, mem *Memory, name string) Core {
 	c := Core{proc: proc, mem: mem}
 	c.icache = NewCache(64, 4, 2)
 	c.dcache = NewCache(64, 4, 2)
+	c.Name = name
 	return c
 }
 
