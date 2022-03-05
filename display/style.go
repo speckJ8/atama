cd30cb255766a0319e2826d53d7d6c2c4b3ef067
Alcides Andrade <andrade.alcides.junior@gmail.com>
2022-03-06 03:04:33 +0900

    common styles and containers

---
+++ display/style.go
@@ -0,0 +1,33 @@
+package display
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	text      = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF"))
+	boldText  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF")).Bold(true)
+	lightText = lipgloss.NewStyle().Foreground(lipgloss.Color("#888888"))
+	greenText = lipgloss.NewStyle().Foreground(lipgloss.Color("#00FF00"))
+	redText   = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
+
+	border = lipgloss.Border{}
+
+	container = lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("#ffffff")).
+			BorderTop(false).
+			BorderBottom(true).
+			BorderLeft(true).
+			BorderRight(true).
+			Padding(0, 1)
+)
+
+func renderWithTitle(s *lipgloss.Style, title string, titlelen int, content string) string {
+	header := fmt.Sprintf("┌─ %s %s┐", title, strings.Repeat("─", s.GetWidth()-titlelen-3))
+	return lipgloss.JoinVertical(lipgloss.Top, header, s.Render(content))
+
+}
